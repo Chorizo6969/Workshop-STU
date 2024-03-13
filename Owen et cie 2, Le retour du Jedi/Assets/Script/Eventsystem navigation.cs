9a880c4b07a5fe7d20d06233a06391dc8f4a54a5
Chorizo <mateoledru05@gmail.com>
2024-03-13 11:58:47 +0100

    Plus de menu

---
+++ Owen et cie 2, Le retour du Jedi/Assets/Script/Eventsystem navigation.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Eventsystemnavigation : MonoBehaviour
+{
+    public GameObject Level_1;
+    //public GameObject Options;
+    public GameObject Weapons;
+    public void Change_Window()
+    {
+        var eventSystem = EventSystem.current;
+        eventSystem.SetSelectedGameObject(Level_1, new BaseEventData(eventSystem));
+    }
+
+    public void Change_Weapons()
+    {
+        var eventSystem = EventSystem.current;
+        eventSystem.SetSelectedGameObject(Level_1, new BaseEventData(eventSystem));
+    }
+
+    /*public void Change_Option()
+    {
+        var eventSystem = EventSystem.current;
+        eventSystem.SetSelectedGameObject(Options, new BaseEventData(eventSystem));
+    }*/
+
+}
